68ed3d6edb79e4089512cc5894488ca9c1903249
James <jamezbrown100@gmail.com>
2017-03-29 20:43:32 -0500

    Create record cell

---
+++ Location Tracker/RecordCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecordCell.swift
+//  Location Tracker
+//
+//  Created by James Brown on 3/29/17.
+//  Copyright © 2017 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class RecordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
