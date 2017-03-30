7d8c275e8b7665ad5c665f605e6d49418b49947f
James <jamezbrown100@gmail.com>
2017-03-30 12:32:53 -0500

    Create text view class

---
+++ Location Tracker/CustomTextView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextView.swift
+//  Location Tracker
+//
+//  Created by James Brown on 3/30/17.
+//  Copyright © 2017 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
