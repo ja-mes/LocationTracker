0c63562857437099680666e09e43d8d6e3ce5d5d
James <jamezbrown100@gmail.com>
2017-03-30 13:31:23 -0500

    Extra spacing on button and update vc

---
+++ Location Tracker/MapVC.swift
@@ -0,0 +1,23 @@
+//
+//  MapVC.swift
+//  Location Tracker
+//
+//  Created by James Brown on 3/30/17.
+//  Copyright © 2017 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
