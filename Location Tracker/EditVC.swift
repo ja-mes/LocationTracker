14364af7a9f345c9dd906e1152e02999678bf275
James <jamezbrown100@gmail.com>
2017-03-30 11:32:51 -0500

    Hook up cancel button

---
+++ Location Tracker/EditVC.swift
@@ -0,0 +1,21 @@
+//
+//  EditVC.swift
+//  Location Tracker
+//
+//  Created by James Brown on 3/30/17.
+//  Copyright © 2017 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class EditVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
