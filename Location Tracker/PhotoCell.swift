3f05d3d233391c26976a4ea94a1e2f0a7cb215b7
James <jamezbrown100@gmail.com>
2017-04-04 16:38:55 -0500

    Create photocell

---
+++ Location Tracker/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  Location Tracker
+//
+//  Created by James Brown on 4/4/17.
+//  Copyright © 2017 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: CustomImageView!
+    
+}
